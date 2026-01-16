4ae08d26baaa0dafe431a4bdfb64a70e885e0872
Alex Wheeldon <eawheeldon@gmail.com>
2026-01-15 22:19:09 -0800

    Remove Flask Variable placeholders

--- src/config.py
+++ src/config.py
@@ -3,7 +3,3 @@ import os
 class Config:
     # Frontend theme selection - set to None for API-only mode
     FRONTEND_THEME = os.environ.get('FRONTEND_THEME', 'basic')
-
-    # Flask config
-    DEBUG = os.environ.get('FLASK_DEBUG', True)
-
